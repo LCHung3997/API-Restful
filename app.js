9189f2d4d2028427a5beace79813728a4eb38c2a
lehung22297 <44271319+lehung22297@users.noreply.github.com>
2019-10-21 16:34:11 +0700

    update middleware

--- app.js
+++ app.js
@@ -24,7 +24,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(passport.initialize());
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next(); 
+});
 app.use('/', indexRouter);
 app.use('/user', auth);
 // app.use('/user', passport.authenticate('jwt', {session: false}), user);
